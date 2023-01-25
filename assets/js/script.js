a9193787db47f6e968ad4f3a30aa18473be5cac2
Sawyer <sawyerolwin@gmail.com>
2023-01-24 22:36:32 -0500

    testing push

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,42 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
+  
+    let heads = 0
+    let tails = 0
+    let total = 0
+    let headsPercent = 0
+    let tailsPercent = 0
+    
+    
     // TODO: Add event listener and handler for flip and clear buttons
-
+document.querySelector('#flip').addEventListener('click', function() {
+    if (Math.random() > .5) {
+        heads++
+        total++
+        headsPercent = (Math.round(heads/total * 100))
+        if (tails > 0) {
+            tailsPercent = (Math.round(tails/total * 100))
+        }
+        document.querySelector('#penny-image').src = 'assets/images/penny-heads.jpg'
+        document.querySelector('#message').innerHTML = 'You flipped heads!'
+        document.querySelector('#heads').innerHTML = heads
+        document.querySelector('#heads-percent').innerHTML = `${headsPercent}%`
+        document.querySelector('#tails-percent').innerHTML = `${tailsPercent}%`
+    } else {
+        tails++
+        total++
+        tailsPercent = (Math.round(tails/total * 100))
+        if (heads > 0) {
+            headsPercent = (Math.round(heads/total * 100))
+        }
+        document.querySelector('#penny-image').src = 'assets/images/penny-tails.jpg'    
+        document.querySelector('#message').innerHTML = 'You flipped tails!'
+        document.querySelector('#tails').innerHTML = tails
+        document.querySelector('#heads-percent').innerHTML = `${headsPercent}%`
+        document.querySelector('#tails-percent').innerHTML = `${tailsPercent}%`
+    }
+})
     // Flip Button Click Handler
         // TODO: Determine flip outcome
         // TODO: Update image and status message in the DOM
@@ -21,6 +51,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     // Clear Button Click Handler
+    document.querySelector('#clear').addEventListener('click', function() {
+        heads = 0
+        tails = 0
+        total = 0
+        headsPercent = 0
+        tailsPercent = 0
+        document.querySelector('#penny-image').src = 'assets/images/penny-heads.jpg'    
+        document.querySelector('#message').innerHTML = 'Let\'s Get Flipping!'
+        document.querySelector('#tails').innerHTML = tails
+        document.querySelector('#heads').innerHTML = heads
+        document.querySelector('#heads-percent').innerHTML = `${headsPercent}%`
+        document.querySelector('#tails-percent').innerHTML = `${tailsPercent}%`
+    })
         // TODO: Reset global variables to 0
         // TODO: Update the scoreboard (same logic as in flip button click handler)
 
